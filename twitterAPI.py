5d44d7c9f41e7ed56fbf49bf2726155a412760a2
benekuehn <41262108+benekuehn@users.noreply.github.com>
2019-11-29 00:27:28 +0000

    Timelimit, print statements, retweeted status, has url,
    Only tweets which are older then one day will be processed.
    Some print statements got updated for better status in terminal.
    Wrong retweet status got fixed.
    is_quote_status got implemented
    has_url got implemented to indicate if tweet consists of a url

--- twitterAPI.py
+++ twitterAPI.py
@@ -22,10 +22,12 @@ tweets_max = 3000
 tweets_per_call = 200
 
 if tweets_max > tweets_per_call:
+    print("More tweets then I can get per call")
     new_tweets = api.user_timeline(
         screen_name=screen_name, count=tweets_per_call, tweet_mode="extended")
 
 elif tweets_max < tweets_per_call:
+    print("Less tweets then I can get per call")
     tweets_per_call = tweets_max
     new_tweets = api.user_timeline(
         screen_name=screen_name, count=tweets_per_call, tweet_mode="extended")
@@ -37,6 +39,8 @@ oldest = Collected_data[-1].id - 1
 tweets_collected = tweets_per_call
 tweets_needed = tweets_max - tweets_collected
 
+print("Still %s tweets to go" % tweets_needed)
+
 while tweets_needed != 0:
 
     if tweets_needed < tweets_per_call:
@@ -45,51 +49,114 @@ while tweets_needed != 0:
     new_tweets = api.user_timeline(
         screen_name=screen_name, count=tweets_needed, max_id=oldest, tweet_mode="extended")
 
+    # wait_on_rate_limit=True
+
     tweets_needed = tweets_needed - tweets_per_call
 
-    print(tweets_collected)
+    print("Still %s tweets to go" % tweets_needed)
 
     Collected_data.extend(new_tweets)
 
     oldest = Collected_data[-1].id - 1
 
-outtweets = [[tweet.id,
-              tweet.user.screen_name,
-              tweet.created_at,
-              tweet.favorite_count,
-              tweet.retweet_count,
-              tweet.retweeted,
-              tweet.source,
-              tweet.text,
-              tweet.in_reply_to_user_id
-             ] for tweet in Collected_data]
+
+# print(Collected_data)
+
+    outtweets = [[tweet.id,
+                  tweet.user.screen_name,
+                  tweet.created_at,
+                  tweet.favorite_count,
+                  tweet.retweet_count,
+                  tweet.source,
+                  tweet.full_text,
+                  # tweet.entities["user_mentions"], // Just in case to memorize how it works
+                  tweet.in_reply_to_user_id,
+                  # tweet.author.screen_name, // Just in case to memorize how it works
+                  ] for tweet in Collected_data if (datetime.now() - tweet.created_at).days > 1] # Only processing tweets which are older then 1 day
+
 
 i = 0
-
+has_writer = False
 for tweet in Collected_data:
-    if "media" in tweet.entities:
-        if "media" in tweet.extended_entities:
-            outtweets[i].append(tweet.extended_entities["media"][0]["type"])
 
-    i += 1
+    # Only processing tweets which are older then 1 day
+    if (datetime.now() - tweet.created_at).days > 1:
+
+        if hasattr(tweet, "retweeted_status"):
+            outtweets[i].append(1)
+        else:
+            outtweets[i].append(0)
+
+        if tweet.is_quote_status == True:
+            outtweets[i].append(1)
+        else:
+            outtweets[i].append(0)
+
+        try:
+            "url" in tweet.entities["urls"][0]
+            outtweets[i].append(1)
+
+        except IndexError:
+            outtweets[i].append(0)
+
+        if "media" in tweet.entities:
+            if "media" in tweet.extended_entities:
+                outtweets[i].append(1)
+        else:
+            outtweets[i].append(0)
+
+        if "^" in tweet.full_text:
+            writer = tweet.full_text.split("^", 1)[1]  # Splits Str at ^
+            # Splits Str again after space to seperate only! two character abbreviation for writer
+            writer = writer.split(" ", 1)[0]
+            outtweets[i].append(writer)
+            has_writer = True
+        i += 1
+
+
+# print(outtweets)
 
 now = datetime.now()
 dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
 
 file_name = "tweets_%s_%s.csv" % (screen_name, dt_string)
 
-with open(file_name, "w",) as f:
-    writer = csv.writer(f)
-    writer.writerow(["tweet_id",
-                     "username",
-                     "created_at",
-                     "favorites",
-                     "retweets",
-                     "retweeted",
-                     "source",
-                     "text",
-                     "in_reply_to_screen_name",
-                     "media_type"])
-    writer.writerows(outtweets)
+# helps to only include writer column in csv only when there is a writer like in my united case.
+if has_writer == True:
+    with open(file_name, "w",) as f:
+        writer = csv.writer(f)
+        writer.writerow(["tweet_id",
+                         "username",
+                         "created_at",
+                         "favorite_count",
+                         "retweet_count",
+                         "source",
+                         "full_text",
+                         "in_reply_to_user_id",
+                         "is_retweeted",
+                         "is_quote_status",
+                         "has_url",
+                         "has_media",
+                         "writer"]
+                        )
+        writer.writerows(outtweets)
 
-pass
+    pass
+else:
+    with open(file_name, "w",) as f:
+        writer = csv.writer(f)
+        writer.writerow(["tweet_id",
+                         "username",
+                         "created_at",
+                         "favorites",
+                         "retweets",
+                         "source",
+                         "text",
+                         "in_reply_to_user_id",
+                         "is_retweeted",
+                         "is_quote_status",
+                         "has_url",
+                         "has_media", ]
+                        )
+        writer.writerows(outtweets)
+    pass
